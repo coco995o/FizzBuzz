09cdbaa10d68b2f8dd62a5edcbb015c15544e6c2
Cosmin <Cosmin@DESKTOP-UTUQF26>
2022-04-27 22:24:42 +0300

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number % 4 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 4 != 0 || number % 3 != 0)
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+        }
+    }
+}
